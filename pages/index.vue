f92a3db92aabd3819a03013e028e93542ce8baa7
Jarek Lipski <loomchild@loomchild.net>
2020-02-18 14:09:52 +0100

    add tertiary

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,48 @@
 <template>
   <v-container full-height>
-    <v-row>
-      <v-col cols="auto">
-        <v-sheet width="100" height="60" color="primary text-center white--text pa-1">
+    <v-row class="text-center">
+      <v-col cols="3">
+        <v-sheet color="primary lighten-3 pa-4 ma-3">
+          Primary Light
+        </v-sheet>
+        <v-sheet color="primary pa-4 ma-3">
           Primary
         </v-sheet>
+        <v-sheet color="primary darken-3 pa-4 ma-3">
+          Primary Dark
+        </v-sheet>
       </v-col>
-      <v-col cols="auto">
-        <v-sheet width="100" height="60" color="secondary text-center white--text pa-1">
+      <v-col cols="3">
+        <v-sheet color="secondary lighten-3 pa-4 ma-3">
+          Secondary Light
+        </v-sheet>
+        <v-sheet color="secondary pa-4 ma-3">
           Secondary
         </v-sheet>
-      </v-col>
-      <v-col cols="auto">
-        <v-sheet width="100" height="60" color="tertiary text-center white--text pa-1">
-          Tertiary
+        <v-sheet color="secondary darken-3 pa-4 ma-3">
+          Secondary Dark
         </v-sheet>
       </v-col>
-      <v-col cols="auto">
-        <v-sheet width="100" height="60" color="accent text-center white--text pa-1">
+      <v-col cols="3">
+        <v-sheet color="tertiary lighten-3 pa-4 ma-3">
+          Tertiary Light
+        </v-sheet>
+        <v-sheet color="tertiary pa-4 ma-3">
+          Tertiary
+        </v-sheet>
+        <v-sheet color="tertiary darken-3 pa-4 ma-3">
+          Tertiary Dark
+        </v-sheet>
+      </v-col>
+      <v-col cols="3">
+        <v-sheet color="accent lighten-3 pa-4 ma-3">
+          Accent Light
+        </v-sheet>
+        <v-sheet color="accent pa-4 ma-3">
           Accent
+        </v-sheet>
+        <v-sheet color="accent darken-3 pa-4 ma-3">
+          Accent Dark
         </v-sheet>
       </v-col>
     </v-row>
