c1b82c639e107c0b51c54e12fc6804ae307c6909
Jarek Lipski <loomchild@loomchild.net>
2020-02-18 14:14:51 +0100

    custom color variants

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,8 +34,16 @@ export default {
       themes: {
         dark: {
           primary: '#4caf50',
-          secondary: '#ff8c00',
-          tertiary: '#4682bf',
+          secondary: {
+            base: '#ff8c00',
+            lighten3: '#ffb700',
+            darken3: '#ff6200'
+          },
+          tertiary: {
+            base: '#4682bf',
+            lighten3: '#4696bf',
+            darken3: '#466ebf'
+          },
           accent: '#9c27b0'
         }
       }
