d8fa787c1c85516eb2201b0187464c5ef6e9215a
Jarek Lipski <loomchild@loomchild.net>
2020-05-24 00:56:24 +0200

    responsive

--- pages/index.vue
+++ pages/index.vue
@@ -7,53 +7,7 @@
         </span>
       </v-col>
     </v-row>
-    <v-row class="text-center">
-      <v-col cols="3">
-        <v-sheet color="primary lighten-3 pa-4 ma-3">
-          Primary Light
-        </v-sheet>
-        <v-sheet color="primary pa-4 ma-3">
-          Primary
-        </v-sheet>
-        <v-sheet color="primary darken-3 pa-4 ma-3">
-          Primary Dark
-        </v-sheet>
-      </v-col>
-      <v-col cols="3">
-        <v-sheet color="secondary lighten-3 pa-4 ma-3">
-          Secondary Light
-        </v-sheet>
-        <v-sheet color="secondary pa-4 ma-3">
-          Secondary
-        </v-sheet>
-        <v-sheet color="secondary darken-3 pa-4 ma-3">
-          Secondary Dark
-        </v-sheet>
-      </v-col>
-      <v-col cols="3">
-        <v-sheet color="tertiary lighten-3 pa-4 ma-3">
-          Tertiary Light
-        </v-sheet>
-        <v-sheet color="tertiary pa-4 ma-3">
-          Tertiary
-        </v-sheet>
-        <v-sheet color="tertiary darken-3 pa-4 ma-3">
-          Tertiary Dark
-        </v-sheet>
-      </v-col>
-      <v-col cols="3">
-        <v-sheet color="accent lighten-3 pa-4 ma-3">
-          Accent Light
-        </v-sheet>
-        <v-sheet color="accent pa-4 ma-3">
-          Accent
-        </v-sheet>
-        <v-sheet color="accent darken-3 pa-4 ma-3">
-          Accent Dark
-        </v-sheet>
-      </v-col>
-    </v-row>
-    <v-row class="mt-4">
+    <v-row class="mt-4 flex-column flex-sm-row align-center">
       <v-spacer />
       <v-col cols="auto">
         <v-btn @click="darken">
@@ -80,6 +34,52 @@
         </v-btn>
       </v-col>
       <v-spacer />
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="12" sm="6" md="3">
+        <v-sheet color="primary lighten-3 pa-4 ma-3">
+          Primary Light
+        </v-sheet>
+        <v-sheet color="primary pa-4 ma-3">
+          Primary
+        </v-sheet>
+        <v-sheet color="primary darken-3 pa-4 ma-3">
+          Primary Dark
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-sheet color="secondary lighten-3 pa-4 ma-3">
+          Secondary Light
+        </v-sheet>
+        <v-sheet color="secondary pa-4 ma-3">
+          Secondary
+        </v-sheet>
+        <v-sheet color="secondary darken-3 pa-4 ma-3">
+          Secondary Dark
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-sheet color="tertiary lighten-3 pa-4 ma-3">
+          Tertiary Light
+        </v-sheet>
+        <v-sheet color="tertiary pa-4 ma-3">
+          Tertiary
+        </v-sheet>
+        <v-sheet color="tertiary darken-3 pa-4 ma-3">
+          Tertiary Dark
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-sheet color="accent lighten-3 pa-4 ma-3">
+          Accent Light
+        </v-sheet>
+        <v-sheet color="accent pa-4 ma-3">
+          Accent
+        </v-sheet>
+        <v-sheet color="accent darken-3 pa-4 ma-3">
+          Accent Dark
+        </v-sheet>
+      </v-col>
     </v-row>
   </v-container>
 </template>
